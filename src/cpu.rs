689ba03d96cf42a939d22e4323cac6b385983c5b
Michael Faißt <michael@codenite.at>
2023-01-03 22:09:11 +0100

    running simple ibm logo test rom works YAY

--- src/cpu.rs
+++ src/cpu.rs
@@ -2,23 +2,26 @@ use crate::font::FONT;
 
 const RAM_SIZE: usize = 4096;
 const STACK_SIZE: usize = 16;
-const VRAM_WIDTH: usize = 64;
-const VRAM_HEIGHT: usize = 32;
+const SCREEN_WIDTH: usize = 64;
+const SCREEN_HEIGHT: usize = 32;
 const NUM_REGISTERS: usize = 16;
+
+type Nibbles = (u16, u16, u16, u16);
 
 pub struct CPU {
     ram: [u8; RAM_SIZE],
-    vram: [[u8; VRAM_WIDTH]; VRAM_HEIGHT],
+    pub vram: [[u8; SCREEN_WIDTH]; SCREEN_HEIGHT],
     stack: [u16; STACK_SIZE],
     pc: usize,
     i: usize,
     delay_timer: u8,
     sound_timer: u8,
-    registers: [u8; NUM_REGISTERS],
+    registers: [u16; NUM_REGISTERS],
 }
 
+#[derive(Debug)]
 pub struct Opcode {
-    nibbles: (u16, u16, u16, u16),
+    nibbles: Nibbles, 
     x: u16,
     y: u16,
     n: u16,
@@ -27,38 +30,122 @@ pub struct Opcode {
 }
 
 impl CPU {
-    pub fn new() -> Self {
+    pub fn new(rom: &Vec<u8>) -> Self {
         let mut ram = [0u8; RAM_SIZE];
 
         for i in 0..FONT.len() {
             ram[0x50 + i] = FONT[i];
         }
 
+        for i in 0..rom.len() {
+            ram[0x200 + i] = rom[i];
+        }
+
         CPU {
-            vram: [[0u8; VRAM_WIDTH]; VRAM_HEIGHT],
+            vram: [[0u8; SCREEN_WIDTH]; SCREEN_HEIGHT],
             ram,
             stack: [0u16; STACK_SIZE],
             i: 0,
             pc: 0x200,
             delay_timer: 0,
             sound_timer: 0,
-            registers: [0u8; NUM_REGISTERS],
+            registers: [0u16; NUM_REGISTERS],
         }
+    }
+
+    fn is_clear_screen_command(nibbles: Nibbles) -> bool {
+        return nibbles.0 == 0x00 && nibbles.1 == 0x00 && nibbles.2 == 0x0E && nibbles.3 == 0x00;
+    }
+
+    fn clear_screen(&mut self) {
+        for i in 0..SCREEN_HEIGHT {
+            for j in 0..SCREEN_WIDTH {
+                self.vram[i][j] = 0x00;
+            }
+        } 
+    }
+
+    fn jump(&mut self, opcode: Opcode) {
+        self.pc = opcode.nnn as usize; 
+    }
+
+    fn set_register(&mut self, opcode: Opcode) {
+        self.registers[opcode.x as usize] = opcode.nn;
+    }
+
+    fn add_to_register(&mut self, opcode: Opcode) {
+        self.registers[opcode.x as usize] += opcode.nn;
+    }
+
+    fn set_index_register(&mut self, opcode: Opcode) {
+        self.i = opcode.nnn as usize; 
+    }
+
+    fn draw(&mut self, opcode: Opcode) {
+        let x = self.registers[opcode.x as usize] as usize;
+        let y = self.registers[opcode.y as usize] as usize;
+        self.registers[0xF] = 0;
+
+        let base = self.i;
+
+        for row in 0..opcode.n {
+            let sprite_data = self.ram[base + row as usize]; 
+            let y_pos = y + row as usize;
+
+            if y_pos >= SCREEN_HEIGHT {
+                break;
+            }
+
+            for i in 0..8 {
+                let x_pos = x + i as usize;
+
+                if x_pos >= SCREEN_WIDTH {
+                    break;
+                }
+
+                let sprite_pixel_on = sprite_data & (0x80 >> i) > 0;
+                let screen_on = self.vram[y_pos][x_pos] > 0;
+
+                if sprite_pixel_on && screen_on {
+                    self.vram[y_pos][x_pos] = 0;
+                    self.registers[0xF] = 1;
+                } else if sprite_pixel_on && !screen_on {
+                    self.vram[y_pos][x_pos] = 1;
+                }
+            }
+        }
+
+
     }
 
     pub fn execute_tick(&mut self) -> Result<(), &str> {
         let opcode = self.get_next_opcode();
 
         match opcode.nibbles {
-            (0x00, 0x00, 0x0e, 0x00) => Ok(()), // 00E0
-            (0x01, 0x4e, 0x4e, 0x4e) => Ok(()), // 1NNN
-            (0x06, 0x58, 0x4e, 0x4e) => Ok(()), // 6XNN
-            (0x07, 0x58, 0x4e, 0x4e) => Ok(()), // 7XNN
-            (0x0a, 0x4e, 0x4e, 0x4e) => Ok(()), // ANNN
-            (0x0d, 0x00, 0x00, 0x00) => Ok(()), // DXYN
+            (0, 0, 0xE, 0) => {
+                if Self::is_clear_screen_command(opcode.nibbles) {
+                    self.clear_screen();
+                }
+            },
+            (0x1, _, _, _) => { 
+                self.jump(opcode);
+            },
+            (0x6, _, _, _) => {
+                self.set_register(opcode);
+            },
+            (0x7, _, _, _) => { 
+                self.add_to_register(opcode);
+            },
+            (0xA, _, _, _) => { 
+                self.set_index_register(opcode);
+            },
+            (0xD, _, _, _) => { 
+                self.draw(opcode);
+            },
+            _ => ()
+        };
 
-            _ => Err("unknown instruction")
-        }
+        Ok(())
     }
 
     pub fn get_next_opcode(&mut self) -> Opcode {
@@ -87,7 +174,4 @@ impl CPU {
         }
     }
 
-    pub fn test(&self) {
-        println!("Hello from the CPU! {:#x}", self.ram[0x050])
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,62 @@
 use core::time;
-use std::thread;
+use std::{thread, fs::read};
+
+use sdl2::{event::Event, render::Canvas, video::Window, pixels::Color, rect::Rect};
 
 mod cpu;
 mod font;
 
+const SCALE: u32 = 10;
+
+
+fn draw_screen(cpu: &cpu::CPU, canvas: &mut Canvas<Window>) {
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+
+    let vram = cpu.vram;
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
+
+    for y in 0..vram.len() {
+        for x in 0..vram[y].len() {
+            if vram[y][x] > 0 {
+                let rect = Rect::new(((x as u32) * SCALE) as i32, ((y as u32) * SCALE) as i32, SCALE, SCALE);
+                canvas.fill_rect(rect).unwrap();
+            }
+        } 
+    }
+
+    canvas.present();
+}
+
 fn main() {
-    let mut cpu = cpu::CPU::new();
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem.window("Chip8 in Rust", 64 * SCALE, 32 * SCALE).position_centered().opengl().build().unwrap();
+    let mut canvas = window.into_canvas().present_vsync().build().unwrap();
+
+    canvas.clear();
+    canvas.present();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    let rom = read("./roms/ibm-logo.ch8").unwrap();
+    let mut cpu = cpu::CPU::new(&rom);
 
     let refresh_rate = time::Duration::from_millis(1000 / 700);
 
-    loop {
-        if !(true) {
-            break;
+    'mainloop: loop {
+        for evt in event_pump.poll_iter() {
+            match evt {
+                Event::Quit{..} => {
+                    break 'mainloop;
+                },
+                _ => ()
+            }
         }
 
         cpu.execute_tick().expect("error during tick");
+        draw_screen(&cpu, &mut canvas);
         thread::sleep(refresh_rate)
     }
 }
